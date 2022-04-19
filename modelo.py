c07e7da37cfe653576ec166f04fca615808b6596
GustavoYoshimiYamashita <gustavo.yamashita@outlook.com>
2022-04-18 22:30:54 -0300

    Adicionando o arquivo modelo.py

---
+++ modelo.py
@@ -0,0 +1,73 @@
+from abc import ABC
+
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome_nome):
+        self._nome = novo_nome.title()
+
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self._likes}"
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes"
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self.temporadas} Temp - {self._likes} Likes"
+
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    def __len__(self):
+        return len(self._programas)
+
+    @property
+    def listagem(self):
+        return self._programas
+
+
+
+vingadores = Filme("Vingadores - guerra infinita", 2018, 160)
+atlanta = Serie("Atlanta", 2018, 2)
+tmep = Filme("Todo mundo em pânico", 1999, 100)
+demolidor = Serie("Demolidor", 2016, 2)
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+print(f"Tamanho da playlist: {len(playlist_fim_de_semana)}")
+
+for programa in playlist_fim_de_semana:
+    print(programa)
+
